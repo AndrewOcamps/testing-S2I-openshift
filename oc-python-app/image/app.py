62b70ee9e12bd3238abd4a2f1cedd0a9844355ce
Gerardo Ocampos <gerardoob97@hotmail.com>
2021-06-19 21:17:01 +0000

    testing GitHub webhooks

--- oc-python-app/image/app.py
+++ oc-python-app/image/app.py
@@ -5,7 +5,7 @@ app = Flask('__file__')
 
 @app.route('/')
 def index():
-    return 'Hello from : ' + socket.gethostname() + '\n'
+    return 'Hello Gerardo WebHooks from : ' + socket.gethostname() + '\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8080')
