687a2acf4e840f43de105294f19fb4530978ea27
developer <developer@rhel180.ocampoge.labs.com>
2022-02-04 16:50:40 -0300

    update app 1

--- simple-flask-app/app.py
+++ simple-flask-app/app.py
@@ -7,7 +7,7 @@ app.config.from_object(ProductionConfig())
 
 @app.route("/")
 def index():
-    return app.config['MSG']
+    return app.config['MSG'] + 'Gerardo Ocampos build 1'
 
 app.run(host='0.0.0.0')
 
