42b53f5e9c8e8a52aec7e0a94b4ade5b10965f4b
Gerardo Ocampos <gerardoob97@hotmail.com>
2022-02-08 11:24:45 -0300

    fix app.py

--- simple-flask-app/app.py
+++ simple-flask-app/app.py
@@ -7,7 +7,7 @@ app.config.from_object(ProductionConfig())
 
 @app.route("/")
 def index():
-    return app.config['MSG'] + 'Gerardo Ocampos build 3
+    return app.config['MSG'] + 'Gerardo Ocampos build 3'
 
 app.run(host='0.0.0.0')
 
