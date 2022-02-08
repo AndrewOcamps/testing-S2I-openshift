1dd1177ba23351d663518be3d8cd79305a1e3318
developer <developer@rhel180.ocampoge.labs.com>
2022-02-08 11:40:50 -0300

    show OS information

--- simple-flask-app/app.py
+++ simple-flask-app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import ProductionConfig
+import platform
+import socket
 
 app = Flask('__file__')
 
@@ -7,7 +9,9 @@ app.config.from_object(ProductionConfig())
 
 @app.route("/")
 def index():
-    return app.config['MSG'] + 'Gerardo Ocampos build 3'
+    os_version = platform.system()
+    os_ipaddr = socket.gethostbyname(socket.gethostname()) 
+    return app.config['MSG'] + os_version  + ' ' +  os_ipaddr
 
 app.run(host='0.0.0.0')
 
